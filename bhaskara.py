927983708d3d990b64901831cd8a6feba772062c
FelipeBritoLC <131112001+FelipeBritoLC@users.noreply.github.com>
2024-05-19 22:54:45 -0300

    Update

--- bhaskara.py
+++ bhaskara.py
@@ -7,7 +7,7 @@ class Variaveis:
     def volume(self):
         return self.a * self.b * self.c
     
-func1 = Variaveis(10, 12, 15)
+func1 = Variaveis(13, 15, 12)
 func2 = Variaveis(9, 17, 11)
 func3 = Variaveis(11, 13, 14)
 
